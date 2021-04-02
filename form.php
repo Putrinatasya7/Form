f410f34c35802c5fcefbfe9020706a8f4059c50d
SoniaRai122 <rai.sonia87@gmail.com>
2021-04-02 21:25:08 +0700

    menambahkan tampilan di form.php

--- form.php
+++ form.php
@@ -5,7 +5,115 @@
 <head>
 
  <title>Form Pendaftaran</title>
+ <style type="text/css">
 
+body {
+    margin: 0;
+    padding: 0;
+    background-color: aqua;
+   }
+
+ * {
+  box-sizing: border-box;
+}
+
+form {
+  padding: 1em;
+  background: #f9f9f9;
+  border: 1px solid #c1c1c1;
+  margin-top: 9rem;
+  max-width: 600px;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 1em;
+}
+
+form input {
+  margin-bottom: 1rem;
+  background: #fff;
+  border: 1px solid #9c9c9c;
+}
+
+form button {
+  background: lightgrey;
+  padding: 0.7em;
+  border: 0;
+}
+
+
+form button:hover {
+  background: green;
+  color: white;
+}
+
+form span a{
+  background: lightblue;
+  padding: 0.7em;
+  border: 0;
+  text-decoration: none;
+  color: black;
+  text-align: center;
+}
+
+
+form span a:hover {
+  background: blue;
+  color: white;
+}
+
+label {
+  text-align: right;
+  display: block;
+  padding: 0.5em 1.5em 0.5em 0;
+}
+
+input {
+  width: 100%;
+  padding: 0.7em;
+  margin-bottom: 0.5rem;
+}
+
+input:focus {
+  outline: 3px solid green;
+}
+
+select {
+  width: 100%;
+  padding: 0.7em;
+  margin-bottom: 0.5rem;
+}
+
+select:focus {
+  outline: 3px solid green;
+}
+
+@media (min-width: 400px) {
+  form {
+    overflow: hidden;
+  }
+
+  label {
+    float: left;
+    width: 200px;
+  }
+
+  input {
+    float: left;
+    width: calc(100% - 200px);
+  }
+
+
+    select {
+    float: left;
+    width: calc(100% - 200px);
+  }
+
+  button {
+    float: right;
+    width: calc(100% - 200px);
+  }
+
+ </style>
 </head>
 
 <body>
